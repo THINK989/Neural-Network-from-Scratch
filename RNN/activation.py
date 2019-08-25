a499b84716a25ccce2d483468bc7fecc8af3fc1e
Raj Shah <raj21101998@gmail.com>
2019-08-25 17:43:25 +0530

    Update activation.py

--- RNN/activation.py
+++ RNN/activation.py
@@ -74,6 +74,28 @@ def Softmax(x):
     return s
 
 
+def relu_backward(dA, cache):
+    
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    Z = cache
+    
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
 
 
 
